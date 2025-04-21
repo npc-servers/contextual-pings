bf66d630f0169219d6826c8aac124210ebfd8887
Jagnt <148128844+Jagnt@users.noreply.github.com>
2025-04-21 20:23:33 -0300

    Allow pinging while dead (#4)
    This seems to only be useful for the intended gamemode (TTT) as to not let people ghost but it works fine with horde as is with this line removed

--- lua/autorun/client/cl_contextual_ping.lua
+++ lua/autorun/client/cl_contextual_ping.lua
@@ -36,8 +36,6 @@ local function PingLocation(isTeam)
     local pingLoc = eye.HitPos
     local pingEnt = eye.Entity
 
-    if !ply:Alive() then return end
-
     if isTTT then
         if ply:IsSpec() then return end
     end
